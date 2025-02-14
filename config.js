646439a62befc87fa7edb225d85ccd53fa8dff0e
paradox <136229133+kavindachamith@users.noreply.github.com>
2025-02-14 10:51:36 +0530

    Update config.js

--- config.js
+++ config.js
@@ -9,8 +9,8 @@ function convertToBool(text, fault = 'true') {
 
 
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || 'session id', //add your session id
-MONGODB: process.env.MONGODB || "mongodb url", //add your mongodb url
+SESSION_ID: process.env.SESSION_ID || 'SUPUN-MD=CMlDgYhS#mAgQ331f3uscwoM7B_AMoV3ExK1Bi9zi37shbBrv6FM', //add your session id
+MONGODB: process.env.MONGODB || "mongodb+srv://kavindachamith13:20060705@cluster0.gcank.mongodb.net", //add your mongodb url
  URL : process.env.BOT_MENU_LINKS || 'https://i.ibb.co/bHXBV08/9242c844b83f7bf9.jpg',
 MAX_SIZE: 500
 };
