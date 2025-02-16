18c90901424200292c059603d15e02c2e9cda760
paradox <136229133+kavindachamith@users.noreply.github.com>
2025-02-16 08:17:30 +0530

    Update config.js

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ function convertToBool(text, fault = 'true') {
 
 
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || 'SUPUN-MD=CMlDgYhS#mAgQ331f3uscwoM7B_AMoV3ExK1Bi9zi37shbBrv6FM', //add your session id
+SESSION_ID: process.env.SESSION_ID || 'SUPUN-MD=7B1kgLYb#AAWx4R6Tm8ZEsyZBH0F3hEzHCsWxUxzfLsY8twh-1No', //add your session id
 MONGODB: process.env.MONGODB || "mongodb+srv://kavindachamith13:20060705@cluster0.gcank.mongodb.net", //add your mongodb url
  URL : process.env.BOT_MENU_LINKS || 'https://i.ibb.co/bHXBV08/9242c844b83f7bf9.jpg',
 MAX_SIZE: 500
